69b00b7d9aca3a20558ec524832ca13cab07b1af
Ed Peters <epeters1111@gmail.com>
2023-01-27 09:25:24 -0500

    Updates pneumatics test code

--- src/main/java/frc/robot/testbots/RobotPneumaticsOnly.java
+++ src/main/java/frc/robot/testbots/RobotPneumaticsOnly.java
@@ -7,44 +7,60 @@ import edu.wpi.first.wpilibj.XboxController;
 
 public class RobotPneumaticsOnly extends TimedRobot {
     
+    public static final int REVPH_CANID = 5;
+
     private XboxController controller;
     private Solenoid pressureReleaseLo;
     private Solenoid pressureReleaseHi;
     private Solenoid clampExtend;
     private Solenoid clampRelease;
+    private boolean prl;
+    private boolean prh;
+    private boolean ext;
+    private boolean rel;
 
     @Override
     public void robotInit() {
+
         controller = new XboxController(0);
-        pressureReleaseLo = new Solenoid(PneumaticsModuleType.REVPH, 14);
-        pressureReleaseHi = new Solenoid(PneumaticsModuleType.REVPH, 14);
-        clampExtend = new Solenoid(PneumaticsModuleType.REVPH, 6);
-        clampRelease = new Solenoid(PneumaticsModuleType.REVPH, 7);
+
+        // TODO - double check that the last parameter (channel assignment) is correct
+        // notice: it's a channel ID on the pneumatics hub, not a CAN ID
+        pressureReleaseLo = new Solenoid(REVPH_CANID, PneumaticsModuleType.REVPH, 14);
+        pressureReleaseHi = new Solenoid(REVPH_CANID, PneumaticsModuleType.REVPH, 14);
+        clampExtend = new Solenoid(REVPH_CANID, PneumaticsModuleType.REVPH, 6);
+        clampRelease = new Solenoid(REVPH_CANID, PneumaticsModuleType.REVPH, 7);
+
+        prl = false;
+        prh = false;
+        ext = false;
+        rel = false;
     }
 
     @Override
     public void teleopPeriodic() {
         
-        // use the buttons to toggle the invidual solenoids
-
+        // A will toggle the low-pressure release solenoid
         if (controller.getAButton()) {
-            pressureReleaseLo.toggle();
+            prl = !prl;
         }
         
+        // B will toggle the high-pressure release solenoid
         if (controller.getBButton()) {
-            pressureReleaseHi.toggle();
+            prh = !prh;
         }
         
+        // X will toggle the extension solenoid
         if (controller.getXButton()) {
-            clampExtend.toggle();
+            ext = !ext;
         }
         
+        // Y will toggle the release solenoid
         if (controller.getYButton()) {
             clampRelease.toggle();
         }
 
-        // try out various combinations of these things
-
+        // TODO try out various combinations of these things, and see which ones work
         if (controller.getLeftBumperPressed()) {
             extendLo();
         }
@@ -56,26 +72,33 @@ public class RobotPneumaticsOnly extends TimedRobot {
         if (controller.getStartButtonPressed()) {
             setNeutral();
         }
+
+        // TODO do solenoids need to be told what to do during every loop, like motors do?
+        pressureReleaseLo.set(prl);
+        pressureReleaseHi.set(prh);
+        clampExtend.set(ext);
+        clampRelease.set(rel);
+
     }
 
     public void setNeutral() {
-        pressureReleaseLo.set(false);
-        pressureReleaseHi.set(false);
-        clampExtend.set(false);
-        clampRelease.set(false);
+        prl = false;
+        prh = false;
+        ext = false;
+        rel = false;
     }
 
     public void extendLo() {
-        pressureReleaseLo.set(true);
-        pressureReleaseHi.set(false);
-        clampExtend.set(true);
-        clampRelease.set(false);
+        prl = true;
+        prh = false;
+        ext = true;
+        rel = false;
     }
 
     public void extendHi() {
-        pressureReleaseLo.set(true);
-        pressureReleaseHi.set(false);
-        clampExtend.set(true);
-        clampRelease.set(false);
+        prl = true;
+        prh = false;
+        ext = true;
+        rel = false;
     }
 }
